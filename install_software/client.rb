bde3e8918af087d01d585c0c5abc6beb8be4f868
Wasim49 <tk606450@gmail.com>
2025-02-04 17:21:58 +0000

    chef workload

--- install_software/client.rb
+++ install_software/client.rb
@@ -1,10 +1,10 @@
-# The below is client.rb file for VM where the automation will be performed, remeber this is for windows VM. 
+# The below is client.rb file for windows workstation/server vm
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "chefusertest"## This name must much the user name on chef io
 client_key               "C:/chef/client.pem"  ## This is user pem key not organsational pem key, this is private key. 
-chef_server_url          "https://chef-automate-6hp2.westus.cloudapp.azure.com/organizations/cheforgtest"
+chef_server_url          "https://chef-automate-ndkh.southcentralus.cloudapp.azure.com/organizations/cheforgtest"
 validation_client_name  "cheforgtest-validator"
 ssl_verify_mode :verify_none      
--- install_software/knife.rb
+++ install_software/knife.rb
@@ -1,7 +1,21 @@
+## The below is for windows workstation/server vm 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "chefuserwk" ## This name must much the user name on chef io, this not the node name of VM or compuetr
-client_key               "/root/scripts/chef/cookbooks/install_software/.chef/wasimcg.pem"  ## This is user pem key not organsational pem key, this is private key.
-chef_server_url          "https://automate.chef.vmware/organizations/cheforgwk"    ## "https://192.168.133.130:443"   or   https://automate.chef.vmware/organizations/cheforgwk
-cookbook_path            "/root/scripts/chef/cookbooks"
+node_name                "chefusertest" ## This name must much the user name on chef io, this not the node name of VM or compuetr
+client_key               "C:/scripts/chef/cookbooks/install_software/.chef/wasimcg.pem"  ## This is user pem key not organsational pem key, this is private key. 
+chef_server_url          "https://chef-automate-ndkh.southcentralus.cloudapp.azure.com/organizations/cheforgtest"    ## "https://api.chef.io/organizations/cheforgcg"     ## 
+cookbook_path            "C:/scripts/chef/cookbooks" 
+
+
+
+
+
+## The below is for ubuntu workstation/server vm 
+# current_dir = File.dirname(__FILE__)
+# log_level                :info
+# log_location             STDOUT
+# node_name                "chefuserwk" ## This name must much the user name on chef io, this not the node name of VM or compuetr
+# client_key               "/root/scripts/chef/cookbooks/install_software/.chef/wasimcg.pem"  ## This is user pem key not organsational pem key, this is private key.
+# chef_server_url          "https://automate.chef.vmware/organizations/cheforgwk"    ## "https://192.168.133.130:443"   or   https://automate.chef.vmware/organizations/cheforgwk
+# cookbook_path            "/root/scripts/chef/cookbooks"
