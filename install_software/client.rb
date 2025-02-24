d20b8c06d7347e713d2552d68c6410f580acbaae
Wasim49 <tk606450@gmail.com>
2025-02-24 17:20:42 +0000

    updated chef

--- install_software/client.rb
+++ install_software/client.rb
@@ -5,6 +5,15 @@ log_level                :info
 log_location             STDOUT
 node_name                "chefusertest"## This name must much the user name on chef io
 client_key               "C:/chef/client.pem"  ## This is user pem key not organsational pem key, this is private key. 
-chef_server_url          "https://172.20.145.124/organizations/cheforgtest"                                                          #"https://172.20.145.124/organizations/cheforgtest"
+chef_server_url          "https://172.20.145.124/organizations/cheforgtest"         #"https://chef-automate-ndkh.southcentralus.cloudapp.azure.com/organizations/cheforgtest"
 validation_client_name  "cheforgtest-validator"
 ssl_verify_mode :verify_none      
+
+
+## The below are linux related client file
+# current_dir = File.dirname(__FILE__)
+# log_level        :info
+# log_location     "/var/log/chef/client.log"
+# chef_server_url  "https://172.20.145.124/organizations/cheforgtest"
+# node_name        "chefusertest"
+# client_key       "/root/.chef/user.pem"
--- install_software/knife.rb
+++ install_software/knife.rb
@@ -12,10 +12,10 @@ ssl_verify_mode :verify_none
 
 
 ## The below is for ubuntu workstation/server vm 
-# current_dir = File.dirname(__FILE__)
 # log_level                :info
 # log_location             STDOUT
-# node_name                "chefuserwk" ## This name must much the user name on chef io, this not the node name of VM or compuetr
-# client_key               "/root/scripts/chef/cookbooks/install_software/.chef/wasimcg.pem"  ## This is user pem key not organsational pem key, this is private key.
-# chef_server_url          "https://automate.chef.vmware/organizations/cheforgwk"    ## "https://192.168.133.130:443"   or   https://automate.chef.vmware/organizations/cheforgwk
-# cookbook_path            "/root/scripts/chef/cookbooks"
+# node_name                "chefusertest"
+# client_key               "/root/.chef/user.pem"
+# chef_server_url          "https://172.20.145.124/organizations/cheforgtest"
+# cookbook_path            "/root/.chef/my_cookbook"
+
